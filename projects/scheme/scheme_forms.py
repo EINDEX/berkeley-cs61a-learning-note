9f38f110373984b02ba6ac8608fc908fb1a8bf8d
EINDEX <eindex.me@outlook.com>
2023-07-23 22:54:41 +0800

    project scheme problem 10

--- projects/scheme/scheme_forms.py
+++ projects/scheme/scheme_forms.py
@@ -50,6 +50,10 @@ def do_define_form(expressions, env):
         # defining a named procedure e.g. (define (f x y) (+ x y))
         # BEGIN PROBLEM 10
         "*** YOUR CODE HERE ***"
+        print("DEBUG: P10: 0", expressions.first, expressions.rest)
+        env.define(signature.first, do_lambda_form(Pair(signature.rest, expressions.rest), env))
+        return signature.first
+
         # END PROBLEM 10
     else:
         bad_signature = signature.first if isinstance(signature, Pair) else signature
